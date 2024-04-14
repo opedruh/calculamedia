8f61ace3bf034c43c7458fa29a3f263b03aa9151
opedruh <148952338+opedruh@users.noreply.github.com>
2024-04-14 20:00:19 -0300

    upload da calculadora de media

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function calcular() {
+    var n1= document.getElementById("n1").value
+    var n2= document.getElementById("n2").value
+    var n3= document.getElementById("n3").value
+
+var media = (Number(n1) + Number(n2) + Number(n3)) / 3
+
+
+alert(`a media dos valores é: ${media}`)
+console.log(`a media dos valores é: ${media}`)}
